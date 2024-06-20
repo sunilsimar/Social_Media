deadb7c0d568dc5efcd97ef88f598f576f11a24e
Sunil <sunisimar2002d@gmail.com>
2024-06-20 22:48:50 +0530

    refactor

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect("mongodb+srv://lalit9025:Lalit9025@cluster0.enuowoj.mongodb.net/Social", {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
 			// To avoid warnings in the console
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
